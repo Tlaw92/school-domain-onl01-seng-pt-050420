497b6db93485de5e89623ff6b3b2d3b8b6b1ec13
travis Lawson <travislawclimb@gmail.com>
2020-06-27 16:14:02 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,35 +1,75 @@
 class School
+  
+  attr_accessor :name, :roster
+  
+ def initialize(name)
+   @name = name 
+   @roster = {}
+ end  
+ 
+ def add_student(name, grade)
+    @roster[grade] = [] if @roster[grade].nil?
+    @roster[grade] << name 
+ end 
+  
+end   
 
-  def initialize(school_name)
-    @school_name = school_name
-    @roster = {}
-  end
 
-  def roster
-    @roster
-  end
 
-  def add_student(name, grade)
-    @student_name = name
-    @grade = grade
-    if @roster.include?(grade) == false
-      @roster[grade] = []
-    end
-    @roster[grade] << name
-  end
 
-  def grade(number)
-    @roster[number]
-  end
 
-  def sort
-    @roster.each do |grade, name|
-      @roster[grade] = name.sort
-    end
-  end
-end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class School
+
+#   def initialize(school_name)
+#     @school_name = school_name
+#     @roster = {}
+#   end
+
+#   def roster
+#     @roster
+#   end
+
+#   def add_student(name, grade)
+#     @student_name = name
+#     @grade = grade
+#     if @roster.include?(grade) == false
+#       @roster[grade] = []
+#     end
+#     @roster[grade] << name
+#   end
+
+#   def grade(number)
+#     @roster[number]
+#   end
+
+#   def sort
+#     @roster.each do |grade, name|
+#       @roster[grade] = name.sort
+#     end
+#   end
+# end
   
 
 
-school = School.new("Bayside High School")
-
+# school = School.new("Bayside High School")
+# school.roster
